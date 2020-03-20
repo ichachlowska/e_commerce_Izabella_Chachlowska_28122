8dac1d4eae5bc85d52a82a47b7e52d378ae01a1a
ichachlowska <61883765+ichachlowska@users.noreply.github.com>
2020-03-20 20:51:59 +0100

    dodanie komponentu

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule, LandingComponent } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+
+import { RouterModule, Routes } from "@angular/router";
+
+const appRoutes: Routes = [
+  { path: "", redirectTo: "/landing", pathMatch: "full" }
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
+
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
