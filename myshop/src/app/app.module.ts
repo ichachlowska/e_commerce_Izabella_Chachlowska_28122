d8ff37644ecda8f32fb1eb114a693f0d90e6bb79
ichachlowska <61883765+ichachlowska@users.noreply.github.com>
2020-03-28 12:37:01 +0100

    Laboratorium 3

--- myshop/src/app/app.module.ts
+++ myshop/src/app/app.module.ts
@@ -1,25 +1,34 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-
-import { LandingComponent } from "./components/landing/landing.component";
-
-import { RouterModule, Routes } from "@angular/router";
-import { ProductsComponent } from "./components/products/products.component";
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProductsComponent } from './components/products/products.component';
+import { PanelTopComponent } from './components/panel-top/panel-top.component';
 
 const appRoutes: Routes = [
-  { path: "products", component: ProductsComponent },
-  { path: "landing", component: LandingComponent },
-  { path: "", redirectTo: "/landing", pathMatch: "full" }
+  { path: 'products', component: ProductsComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: '', redirectTo: '/landing', pathMatch: 'full' }
 ];
 
 @NgModule({
-  declarations: [AppComponent, ProductsComponent, LandingComponent],
-  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(appRoutes)],
-
+  declarations: [
+    AppComponent,
+    LandingComponent,
+    ProductsComponent,
+    PanelTopComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
